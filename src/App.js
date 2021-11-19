8a4253f17af0d73a7c10a8938f36804ff70fea07
ShirleyyFang <yanbingfang@utexas.edu>
2021-11-18 16:42:40 -0800

    migrate the original code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Profile from './pages/profile';
+import Home from './pages/home';
+import Cases from './pages/casesInterface';
+import { Fragment } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+        <Fragment>
+          <BrowserRouter>
+          <Routes>
+            <Route path = '/' element={<Home />}></Route>
+            <Route path = '/cases' element={<Cases/>}></Route>
+            <Route path = '/profile' element={<Profile/>}></Route>
+          </Routes>
+          </BrowserRouter>
+        </Fragment>
+    )
+  }
 }
-
 export default App;
