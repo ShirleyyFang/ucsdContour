83acff6797f22ccd30eda0e4a212f1b27913986f
ShirleyyFang <yanbingfang@utexas.edu>
2022-03-28 11:46:31 -0700

    Update casesInterface.js

--- src/pages/casesInterface.js
+++ src/pages/casesInterface.js
@@ -10,13 +10,6 @@ const {Header, Content} = Layout;
 const {Search} = Input;
 const onSearch = value => console.log(value);
 
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-];
 
 const cases = {
   "Head and Neck": 
